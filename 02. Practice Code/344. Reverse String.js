39855699e5fc9e4c92db930015fe72fe8e4d1aa2
jay1patel1 <140941387+jay1patel1@users.noreply.github.com>
2024-02-19 13:57:17 +0000

    updated

--- 02. Practice Code/344. Reverse String.js
+++ 02. Practice Code/344. Reverse String.js
@@ -3,18 +3,17 @@ function reverseString (string) {
     let pointer_a = 0;
     let pointer_b = string.length
 
-    console.log(pointer_a,pointer_b); 
-
     while (pointer_a <= pointer_b) {
         let tempValue = value[pointer_a];
         value[pointer_a] = value[pointer_b];
         value[pointer_b] = tempValue;
         pointer_a++;
         pointer_b--;
-        console.log(value);
     }
 
+    value = value.join("")
     console.log(value);
+    return value
 }
 
-reverseString ("string")
+reverseString ("string");
