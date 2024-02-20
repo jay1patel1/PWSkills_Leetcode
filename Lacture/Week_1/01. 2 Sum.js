8a3938d9c9a4a78db20f09dfcf7ee263eb903c1d
jay1patel1 <140941387+jay1patel1@users.noreply.github.com>
2024-02-20 20:11:28 +0530

    updated

---
+++ Lacture/Week_1/01. 2 Sum.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+let twoSum = function(nums, target) {
+    for (let i = 0; i <= nums.length-2; i++) {
+        for (let j = i+1; j < nums.length; j++) {
+            if (nums[i] + nums [j] === target) {
+                return [nums[i],nums[j]];
+            }
+        }
+    }
+};
+
+twoSum([2,5,6,7,8,10], 16);
