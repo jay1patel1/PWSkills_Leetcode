b34fb6614ea5de3bb954475dbd4b49943791bd31
jay1patel1 <140941387+jay1patel1@users.noreply.github.com>
2024-02-22 04:09:10 +0000

    updated

---
+++ Homework/Week-1/151. Reverse Words in a String.js
@@ -0,0 +1,13 @@
+/**
+ * @param {string} s
+ * @return {string}
+ */
+const reverseWords = function(s) {
+    let array = s.split (" ");
+    array = ar
+    console.log(array);
+};
+
+reverseWords ("the sky is blue");
+reverseWords ("  hello world  ");
+reverseWords ("a good   example");
