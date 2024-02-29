361365246a55f9e85a79d818aae693c5c34130a6
jay1patel1 <140941387+jay1patel1@users.noreply.github.com>
2024-02-29 22:33:07 +0530

    Update 219. Contains Duplicate II.js

--- 02. Practice Code/219. Contains Duplicate II.js
+++ 02. Practice Code/219. Contains Duplicate II.js
@@ -5,21 +5,17 @@
  */
 var containsNearbyDuplicate = function(nums, k) {
 
-    for (let i = 0; i < (nums.length-1); i++) {
-        for (let j = i+1; j < nums.length; j++) {
-            if (nums[i] === nums[j]) {
-                let value = j-i;
-                console.log(i,j,value);
-                
-                if (value > k) {
-                    console.log(false);
-                }
-            }
+    const map = new Map ();
+    for (let i = 0; i < nums.length; i++) {
+        if (i - map.get(nums[i]) <= k) {
+            console.log("true");
+            return 0;
         }
-        
+        map.set(nums[i],i);
     }
+    console.log("false") ;
 };
 
 containsNearbyDuplicate ([1,2,3,1],3);
 containsNearbyDuplicate ([1,0,1,1],1);
-containsNearbyDuplicate ([1,2,3,1,2,3],2);
+containsNearbyDuplicate ([1,2,1,3,2,3,1],2);
