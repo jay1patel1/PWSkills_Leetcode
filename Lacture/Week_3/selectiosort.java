1c1aa6ef0363f4d9f31f1d74edc91d7363666d56
jay1patel1 <140941387+jay1patel1@users.noreply.github.com>
2024-03-05 22:49:07 +0530

    updated

---
+++ Lacture/Week_3/selectiosort.java
@@ -0,0 +1,9 @@
+public class selectiosort {
+    public static int[] sortingArray (int[] args) {
+        
+    }
+    public static void main(String[] args) {
+        int[] arr = {5,3,4,6,4,6,3,8,9,10};
+        sortingArray(arr);
+    }
+}
