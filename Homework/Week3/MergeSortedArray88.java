5a62b0fa4f44fe6d12c9e1e968867d0411b98286
jay1patel1 <140941387+jay1patel1@users.noreply.github.com>
2024-03-06 15:13:11 +0530

    Update MergeSortedArray88.java
    updated

--- Homework/Week3/MergeSortedArray88.java
+++ Homework/Week3/MergeSortedArray88.java
@@ -1,42 +1,34 @@
-///**
-// * MergeSortedArray88
-// */
-//public class MergeSortedArray88 {
-//
-//    public void merge(int[] nums1, int m, int[] nums2, int n) {
-//
-//
-//
-//        for (int i = 0; i < n; i++) {
-//            int keyPoint = m;
-//            while () {
-//                if (nums1[keyPoint+i] > nums2[i]) {
-//                    int tempValue = nums1[keyPoint+i];
-//                    nums1[keyPoint+i] = nums1[keyPoint+i+1];
-//                    nums1[keyPoint+i] = tempValue;
-//                    keyPoint--;
-//                } else {
-//                    nums1[keyPoint+i-1] = nums2[i];
-//                }
-//            }
-//
-//        }
-//    }
-//
-//    public static void main(String[] args) {
-//        MergeSortedArray88 merger = new MergeSortedArray88();
-//        int[] nums1 = {1,2,3,0,0,0};
-//        int[] nums2 = {2,5,6};
-//        merger.merge(nums1, 3, nums2, 3);
-//
-//        MergeSortedArray88 merger2 = new MergeSortedArray88();
-//        int[] nums1_2 = {1};
-//        int[] nums2_2 = {};
-//        merger.merge(nums1_2, 1, nums2_2, 0);
-//
-//        MergeSortedArray88 merger3 = new MergeSortedArray88();
-//        int[] nums1_3 = {0};
-//        int[] nums2_3 = {1};
-//        merger.merge(nums1_3, 0, nums2_3, 1);
-//    }
-//}
+/**
+ * MergeSortedArray88
+ */
+public class MergeSortedArray88 {
+
+    public void merge(int[] nums1, int m, int[] nums2, int n) {
+        int p_a = nums1.length-1;
+        int p_b = m-1;
+        int p_c = nums2.length-1;
+
+        while (nums1[p_b] > nums2[p_c] {
+            if (nums1[p_b] > nums2[p_c]) {
+                int tmep = nums1[p_b];
+                nums2[p_c]
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] nums1 = { 1, 2, 3, 0, 0, 0 };
+        int[] nums2 = { 2, 5, 6 };
+        merge(nums1, 3, nums2, 3);
+
+        int[] nums1_2 = { 1 };
+        int[] nums2_2 = {};
+        merge(nums1_2, 1, nums2_2, 0);
+
+        MergeSortedArray88 merger3 = new MergeSortedArray88();
+        int[] nums1_3 = { 0 };
+        int[] nums2_3 = { 1 };
+        merge(nums1_3, 0, nums2_3, 1);
+    }
+
+   };
