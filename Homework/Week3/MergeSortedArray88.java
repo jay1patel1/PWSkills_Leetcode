572079b1832515facf4a012c2760966bc6ff6790
jay1patel1 <140941387+jay1patel1@users.noreply.github.com>
2024-03-06 22:54:15 +0530

    updated

--- Homework/Week3/MergeSortedArray88.java
+++ Homework/Week3/MergeSortedArray88.java
@@ -6,33 +6,30 @@ import java.util.Arrays;
 public class MergeSortedArray88 {
 
     public static void merge(int[] nums1, int m, int[] nums2, int n) {
-        int p_a = nums1.length-1;
-        int p_b = m-1;
-        int p_c = nums2.length-1;
+        int i = m - 1; // Pointer for nums1
+        int j = n - 1; // Pointer for nums2
+        int k = m + n - 1; // Pointer for the merged array
 
-        while (nums1[p_b] > nums1[p_a]) {
-            if (nums1[p_b] > nums2[p_c]) {
-                int temp = nums1[p_b];
-                nums1[p_b] = nums1[p_a];
-                nums1[p_a] = temp;
-                p_b--;
-                p_a--;
+        while (i >= 0 && j >= 0) {
+            if (nums1[i] > nums2[j]) {
+                nums1[k--] = nums1[i--];
             } else {
-                int temp = nums2[p_c];
-                nums2[p_c] = nums1[p_a];
-                nums1[p_a] = temp;
-                p_a--;
-                p_c--;
+                nums1[k--] = nums2[j--];
             }
+        }
+
+        // If there are remaining elements in nums2, add them to nums1
+        while (j >= 0) {
+            nums1[k--] = nums2[j--];
         }
 
         System.out.println(Arrays.toString(nums1));
     }
 
     public static void main(String[] args) {
-        int[] nums1 = { 1, 2, 3, 0, 0, 0 };
-        int[] nums2 = { 2, 5, 6 };
-        merge(nums1, 3, nums2, 3);
+        int[] nums1 = { 1, 2, 3, 10, 12, 14, 0 , 0, 0, 0, 0};
+        int[] nums2 = { 2, 5, 6, 7, 8 };
+        merge(nums1, 6, nums2, 5);
 
         int[] nums1_2 = { 1 };
         int[] nums2_2 = {};
