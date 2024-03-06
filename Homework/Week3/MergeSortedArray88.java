4241a7e58e60408fda7c88d6cbfa71249c4fac2a
jay1patel1 <140941387+jay1patel1@users.noreply.github.com>
2024-03-06 21:26:04 +0530

    updated

--- Homework/Week3/MergeSortedArray88.java
+++ Homework/Week3/MergeSortedArray88.java
@@ -1,19 +1,32 @@
+import java.util.Arrays;
+
 /**
  * MergeSortedArray88
  */
 public class MergeSortedArray88 {
 
-    public void merge(int[] nums1, int m, int[] nums2, int n) {
+    public static void merge(int[] nums1, int m, int[] nums2, int n) {
         int p_a = nums1.length-1;
         int p_b = m-1;
         int p_c = nums2.length-1;
 
-        while (nums1[p_b] > nums2[p_c] {
+        while (nums1[p_b] > nums1[p_a]) {
             if (nums1[p_b] > nums2[p_c]) {
-                int tmep = nums1[p_b];
-                nums2[p_c]
+                int temp = nums1[p_b];
+                nums1[p_b] = nums1[p_a];
+                nums1[p_a] = temp;
+                p_b--;
+                p_a--;
+            } else {
+                int temp = nums2[p_c];
+                nums2[p_c] = nums1[p_a];
+                nums1[p_a] = temp;
+                p_a--;
+                p_c--;
             }
         }
+
+        System.out.println(Arrays.toString(nums1));
     }
 
     public static void main(String[] args) {
@@ -25,7 +38,6 @@ public class MergeSortedArray88 {
         int[] nums2_2 = {};
         merge(nums1_2, 1, nums2_2, 0);
 
-        MergeSortedArray88 merger3 = new MergeSortedArray88();
         int[] nums1_3 = { 0 };
         int[] nums2_3 = { 1 };
         merge(nums1_3, 0, nums2_3, 1);
