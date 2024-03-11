4262ef61dc25b07e641b0f1d855dd617d639f2a5
jay1patel1 <140941387+jay1patel1@users.noreply.github.com>
2024-03-11 22:54:24 +0530

    SelectionSortRecursion.js updated

--- Homework/Week4/SelectionSortRecursion.js
+++ Homework/Week4/SelectionSortRecursion.js
@@ -1,43 +1,42 @@
 function selectionSortRecursion (arr) {
 
-    function sort (arr,a,b,c) {
-        
-        if (c == 0) {
-            return arr;
-        }
+    function sort(arr,search,high,bucket) {
 
-        if (a === c) {
-            [arr[b], arr[c]] = [arr[c], arr[b]];
-            c--;
-            a = 0;
-            b = 0;
+        if (bucket < 1 ) {
+            return;
         } else {
-            
+
+            if (search < bucket) {
+                if (arr[search] > arr[high]) {
+                    high = search;
+                    search++;
+                } else {
+                    search++;
+                }
+            } else {
+                bucket--;
+                [arr[high],arr[bucket]] = [arr[bucket],arr[high]];
+                high = 0;
+                search = 0;
+            }
         }
 
-        if (arr[a] >= arr [b]) {
-            b = a;
-            a++;
-        } else {
-            a++;
-        }
+        sort(arr,search,high,bucket);
 
-        sort (arr,a,b,c);
         return arr;
+
     }
 
-    let retrunArray = sort (arr,0,0,arr.length-1);
-
-    return retrunArray;
-
+    return sort(arr, 0, 0, arr.length);
 }
 
 console.log(selectionSortRecursion([5,2,1,8,10,4,3,1]));
-// console.log(bubbleSortRecursion([64, 34, 25, 12, 22, 11, 90]));
-// console.log(bubbleSortRecursion([4, 1, 3, 9, 7]));
-// console.log(bubbleSortRecursion([-4, -1, -3, -9, -7]));
-// console.log(bubbleSortRecursion([-4, -1, -3, -9, -7]));
-// console.log(bubbleSortRecursion([1,1,1,1,1,1,1]));
-// console.log(bubbleSortRecursion([1,0,1,0,1,0,1]));
-// console.log(bubbleSortRecursion([-1,0,-1,0,-1,0,-1]));
-// console.log(bubbleSortRecursion([-1,8,-4,78,-45456456,7,-456456456456]));
+console.log(selectionSortRecursion([4,3,1]));
+console.log(selectionSortRecursion([64, 34, 25, 12, 22, 11, 90]));
+console.log(selectionSortRecursion([4, 1, 3, 9, 7]));
+console.log(selectionSortRecursion([-4, -1, -3, -9, -7]));
+console.log(selectionSortRecursion([-4, -1, -3, -9, -7]));
+console.log(selectionSortRecursion([1,1,1,1,1,1,1]));
+console.log(selectionSortRecursion([1,0,1,0,1,0,1]));
+console.log(selectionSortRecursion([-1,0,-1,0,-1,0,-1]));
+console.log(selectionSortRecursion([-1,8,-4,78,-45456456,7,-456456456456]));
