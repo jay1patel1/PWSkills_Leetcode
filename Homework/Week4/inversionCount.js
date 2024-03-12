99cb8d16c86505a1454349660775cd43c1fd6357
jay1patel1 <140941387+jay1patel1@users.noreply.github.com>
2024-03-12 20:38:47 +0530

    inversionCount.js is done

--- Homework/Week4/inversionCount.js
+++ Homework/Week4/inversionCount.js
@@ -8,7 +8,7 @@ function mergeSort (arr) {
 
         while (i < left.length && j < right.length) {
             if (left[i] > right[j]) {
-                inversion++;
+                inversion += left.length-i;
             }
             if (left[i] <= right[j]) {
                 resultArray.push(left[i]);
@@ -45,11 +45,15 @@ function mergeSort (arr) {
         return merge (left,right)
     }
 
-    helper(arr,0,arr.length-1,0);
+    let a = helper(arr,0,arr.length-1,0);
+    console.log(a);
     return inversion;
 
 }
 
 console.log(mergeSort([1, 20, 6, 4, 5]))
+console.log(mergeSort([5,3,2,4,1]))
 console.log(mergeSort([8, 4, 2, 1]))
 console.log(mergeSort([2, 4, 1, 3, 5]))
+console.log(mergeSort([2, 3,4,5,6]))
+console.log(mergeSort([10,10,10]))
