4004a77b8b62bc278ad546bb75fac828f6ef3b3f
jay1patel1 <140941387+jay1patel1@users.noreply.github.com>
2024-03-14 15:44:22 +0000

    updated

--- Homework/Week4/69. Sqrt(x).js
+++ Homework/Week4/69. Sqrt(x).js
@@ -3,5 +3,28 @@
  * @return {number}
  */
 var mySqrt = function(x) {
-    
+    function binarySearch (target,start,end) {
+        let value = 
+        if (start > end) {
+            return -1;
+        } else {
+            const mid = Math.floor((start+end) / 2);
+            
+            if ((end - mid) === target) {
+                    return mid;
+            } else if ((end - mid) > target) {
+                binarySearch (target, start, mid);
+            }  else {
+                binarySearch (target, mid, end);
+            }
+        }
+    }
+
+    let target = 0;
+    let start = 0;
+    let end = x;
+
+    binarySearch (target,start,end);
 };
+
+mySqrt (16);
