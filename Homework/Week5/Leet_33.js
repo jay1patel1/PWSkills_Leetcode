4480f96f2eb4b00148a8d5b3a0ddef00d810abe1
jay1patel1 <140941387+jay1patel1@users.noreply.github.com>
2024-03-19 01:21:13 +0530

    rotatedArrayMin.js done

--- Homework/Week5/Leet_33.js
+++ Homework/Week5/Leet_33.js
@@ -9,18 +9,17 @@ function search (nums,target) {
             return mid;
         }
 
-
-        if (nums[lo] <= nums[mid]) {
-            if (nums[lo] <= target && target < nums[mid]) {
+        if (nums[lo] <= nums[mid]){
+            if (nums[lo] <= target && nums[mid] >= target) {
                 hi = mid-1;
             } else {
                 lo = mid + 1;
             }
         } else {
-            if (nums[mid] < target && target <= nums[hi]) {
-                lo = mid + 1;
+            if (nums[mid] <= target && nums[hi] >= target) {
+                lo = mid+1;
             } else {
-                hi = mid - 1;
+                hi = mid-1;
             }
         }
     }
--- Homework/Week5/rotatedArrayMin.js
+++ Homework/Week5/rotatedArrayMin.js
@@ -15,12 +15,22 @@ var findMin = function(nums) {
             return mid;
         } else if (nums[mid] < nums[mid-1] && mid === nums.length-1){
             return mid;
-        } else if (nums.length === 1 && nums[0]) {
+        } else if (nums.length === 1) {
             return mid;
         }
 
-        if (nums[lo] <= nums[mid]) {
-            
+        if (nums[lo] <= nums[mid] && nums[mid] > nums[hi]) {
+            lo = mid+1;
+        } else {
+            hi = mid-1;
         }
     }
 };
+
+console.log(findMin([3,4,5,1,2]))
+console.log(findMin([4,5,6,7,0,1,2]))
+console.log(findMin([11,13,15,17]))
+console.log(findMin([0]))
+console.log(findMin([20,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19]))
+console.log(findMin([1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19]))
+console.log(findMin([5,2]))
