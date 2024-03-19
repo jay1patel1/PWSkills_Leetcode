bf2f733fe13a56515e2493b2c243004e7bb1e4ff
jay1patel1 <140941387+jay1patel1@users.noreply.github.com>
2024-03-19 03:49:29 +0000

    updated

---
+++ Homework/Week5/PeakIndexInMountainArray.js
@@ -0,0 +1,11 @@
+/**
+ * @param {number[]} arr
+ * @return {number}
+ */
+var peakIndexInMountainArray = function(arr) {
+    
+};
+
+console.log(peakIndexInMountainArray([0,1,0]));
+console.log(peakIndexInMountainArray([0,2,1,0]));
+console.log(peakIndexInMountainArray([0,10,5,2]));
